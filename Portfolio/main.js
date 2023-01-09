28ad96c3b6d3185c3ac6f584ac9256e969ee7259
Wladimir Acevedo <wladimir.acevedo.237@gmail.com>
2023-01-09 20:04:03 -0300

    1

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,71 +1,11 @@
-function animate({ timing, draw, duration }) {
-  let start = performance.now();
-
-  requestAnimationFrame(function animate(time) {
-    // timeFraction va de 0 a 1
-    let timeFraction = (time - start) / duration;
-    if (timeFraction > 1) timeFraction = 1;
-
-    // calcular el estado actual de la animación
-    let progress = timing(timeFraction);
-
-    draw(progress); // dibujar
-
-    if (timeFraction < 1) {
-      requestAnimationFrame(animate);
-    }
-  });
-}
-
-
-
-function animateText(textArea) {
-  document.getElementById("textExample").innerHTML = "Bienvenido <3";
-  $( '.name' ).addClass("animate1 ")
-  setTimeout(function () {
-    window.location.href = "http://127.0.0.1:5500/me.html";
-  }, 5000);
-  
- 
-
-  let text = textArea.value;
-  let to = text.length,
-    from = 0;
-
-  animate({
-    duration: 2000,
-    timing: bounce,
-    draw: function (progress) {
-      let result = (to - from) * progress + from;
-      textArea.value = text.slice(0, Math.ceil(result));
-      
-    },
-    setTimeout() {},
-    
-    
-    
-  })
-  
-  
-  
-}
-
-function bounce(timeFraction) {
-  for (let a = 0, b = 1; 1; a += b, b /= 2) {
-    if (timeFraction >= (7 - 4 * a) / 11) {
-      return (
-        -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
-      );
-    }
-  }
-}
-
 function hiden() {
   var r = document.getElementById("m");
   r.removeAttribute("hidden");
 
   $( '#btnHide' ).addClass("slideOut ")
   $( '#btnHide' ).hide(2000)
+  $( '#sectp' ).hide(2000)
+
  
 
   
